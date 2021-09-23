0c476e5cb8569b6aefc9dd98d403b3ceaafaf6a0
SobolevvMaxim <brtgfc@bk.ru>
2021-09-24 00:07:15 +0300

    done

--- src/com/company/Run.java
+++ src/com/company/Run.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Run {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        char Char = input.toCharArray()[0];
+//        byte[] bytes = input.getBytes("Windows-1253");
+        System.out.println("16: " + String.format("%04x", (int) Char));
+        System.out.println("8: " + String.format("%02x", (int) Char));
     }
 }
